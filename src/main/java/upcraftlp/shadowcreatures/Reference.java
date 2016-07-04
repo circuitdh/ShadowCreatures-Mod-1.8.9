56539667e48502ca11bf1187688fa09270a92d03
UpcraftLP <none>
2016-07-04 14:38:49 +0200

    new Update-URL

--- src/main/java/upcraftlp/shadowcreatures/Reference.java
+++ src/main/java/upcraftlp/shadowcreatures/Reference.java
@@ -7,6 +7,6 @@ public class Reference {
 	public static final String VERSION = "0.0.6a";
 	public static final String CLIENT_PROXY_CLASS = "upcraftlp.shadowcreatures.proxy.ClientProxy";
 	public static final String SERVER_PROXY_CLASS = "upcraftlp.shadowcreatures.proxy.CommonProxy";
-	public static final String INTERNAL_UPDATE_URL = "";
+	public static final String INTERNAL_UPDATE_URL = "https://raw.githubusercontent.com/UpcraftLP/ShadowCreatures-Mod-1.8.9/master/Version.txt";
 	
 }
--- src/main/java/upcraftlp/shadowcreatures/events/PlayerEvents.java
+++ src/main/java/upcraftlp/shadowcreatures/events/PlayerEvents.java
@@ -17,7 +17,4 @@ public class PlayerEvents {
 			event.setCanceled(true);
 		}
 	}
-	
-	
-	
 }
